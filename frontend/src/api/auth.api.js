36fce6e10d6371a2ab58cf725a0f0e52fe72d06a
thupx <xuanthu404@gmail.com>
2020-05-07 15:54:12 +0700

    refactor

--- frontend/src/api/auth.api.js
+++ frontend/src/api/auth.api.js
@@ -1,7 +1,5 @@
 import fetch from 'cross-fetch';
-import URI, { HOST } from './config';
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+import URI, { HOST, headers } from './config';
 export const requestLogin = async (body) => {
     var urlencoded = new URLSearchParams();
     urlencoded.append("username", body.username);
@@ -9,7 +7,7 @@ export const requestLogin = async (body) => {
     const response = await fetch(`${HOST}/${URI.AUTH_URI.JWT}/create`, {
         method: 'post',
         body: urlencoded,
-        headers: myHeaders
+        headers: headers
     });
     const json = await response.json();
     if (!json.access) throw json;
