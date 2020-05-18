20203b4510c5cfa266d68cc9dabb4ec9682da252
thupx <xuanthu404@gmail.com>
2020-05-18 17:38:54 +0700

    refactor(FE)

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -4,18 +4,20 @@ import { Switch } from 'react-router';
 import Login from '../components/Login';
 import App from '../App';
 import Register from '../components/Register';
-import Dashboard from '../pages/Dashboard';
+import DashboardPage from '../pages/Dashboard';
 import Transactions from '../components/Transaction';
 import PublicRoute from './PublicRoute';
 import PrivateRoute from './PrivateRoute';
+import CategoryPage from '../pages/Categories';
 const routes = (
     <Switch>
         <PublicRoute component={Home} path="/" exact />
         <PublicRoute restricted={true} path="/login" component={Login} />
         <PublicRoute path="/about" component={App} />
         <PublicRoute restricted={true} path="/register" component={Register} />
-        <PrivateRoute path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/dashboard" component={DashboardPage} />
         <PrivateRoute path="/transaction" component={Transactions} />
+        <PrivateRoute path="/category" component={CategoryPage} />
     </Switch>
 )
 export default routes;
