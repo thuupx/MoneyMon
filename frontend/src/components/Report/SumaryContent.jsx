a2088d7d24fd5f709412a394cc522f1c250413c2
thupx <xuanthu404@gmail.com>
2020-06-09 07:25:53 +0700

    feat(FE): add chart view transaction by cat

--- frontend/src/components/Report/SumaryContent.jsx
+++ frontend/src/components/Report/SumaryContent.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import { Row, Col, Statistic, Progress } from 'antd';
 import CanvasJSReact from '../../libs/canvasjs.react';
 import { useEffect } from 'react';
+import filter from 'lodash/filter';
+import mergeWith from 'lodash/mergeWith';
 import { getTransactionsFromWallet } from '../../api';
-const CanvasJS = CanvasJSReact.CanvasJS;
+import { mergeDupTransactionByCategory } from '../../utils/transaction.util';
+// const CanvasJS = CanvasJSReact.CanvasJS;
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 // const initData = 
-const initOptions = (dataPoints) => ({
+const setOptions = (dataPoints) => ({
     animationEnabled: true,
     exportEnabled: true,
     theme: "dark2", // "light1", "dark1", "dark2"
@@ -25,44 +28,59 @@ export default function SummaryContent(props) {
     const { wallet } = props;
     const [transactionsWallet, setTransactionsWallet] = useState([]);
     const [dataPoints, setDataPoints] = useState([{ y: 100, label: "Category Name" }]);
-    const [options, setOptions] = useState(initOptions({ y: 100, label: "Category Name" }));
-    console.log("SummaryContent -> wallet", wallet);
+    // const [options, setOptions] = useState(initOptions({ y: 100, label: "Category Name" }));
     useEffect(() => {
         //fetch all transactions of the wallet
         const fetchTransOfWallet = async () => {
             const listTransactions = await getTransactionsFromWallet(wallet.id);
+            //remove init wallet transactions
+            const idx = listTransactions.findIndex(tran => tran.category === 1);
+            listTransactions.splice(idx, 1);
             console.log("fetchTransOfWal -> listTransactions", listTransactions)
             setTransactionsWallet(listTransactions);
-            const newDataPoints = listTransactions.map(tran => {
-                let sign = "";
-                if (tran.action === "IN") sign = "+"
-                else sign = "-";
-                return { label: tran.category_name, y: `${sign}${tran.amount}` };
-            });
-            // const existCat = []
-            // listTransactions.forEach(tran => {
-            //     if (!existCat.includes(tran["category_name"]))
-            //         existCat.push(tran["category_name"])
-            // });
-            // console.log("fetchTransOfWal -> existCat", existCat)
 
-            const rest =
-                listTransactions
-                    .map(tran => tran.action === "IN" ? parseFloat(tran.amount) : -parseFloat(tran.amount))
-                    .reduce((acc, curr) => acc + curr, 0);
-
-            const used = parseFloat(wallet.balance) - rest;
-            console.log("fetchTransOfWal -> wallet.balance", wallet.balance)
-            setDataPoints([...newDataPoints, { y: rest, label: "Balance" }]);
         }
         fetchTransOfWallet();
     }, [])
 
+    useEffect(() => {
+        const tempArray = [];
+        const duplicateCat = [];
+        transactionsWallet.forEach((tran => {
+
+            if (tempArray.includes(tran.category) && !duplicateCat.includes(tran.category))
+                duplicateCat.push(tran.category);
+            tempArray.push(tran.category);
+        }));
+        console.log("fetchTransOfWal -> duplicateCat", duplicateCat);
+        let newDataPoints = [];
+        transactionsWallet.forEach(tran => {
+            // !tempArray.includes(tran.category);
+            if (!duplicateCat.includes(tran.category)) {
+                let sign = "";
+                if (tran.action === "IN") sign = "+"
+                else sign = "-";
+                newDataPoints.push({ label: tran.category_name, y: `${sign}${tran.amount}` })
+            }
+        });
+        console.log("fetchTransOfWal -> newDataPoints", newDataPoints)
+
+        const dataPointsMerged = mergeDupTransactionByCategory(transactionsWallet, duplicateCat);
+        console.log("fetchTransOfWal -> dataPointsMerged", dataPointsMerged)
+        const rest =
+            transactionsWallet
+                .map(tran => tran.action === "IN" ? parseFloat(tran.amount) : -parseFloat(tran.amount))
+                .reduce((acc, curr) => acc + curr, 0);
+
+        const used = parseFloat(wallet.balance) + rest;
+        // if(newDataPoints.length===0) 
+        setDataPoints([...newDataPoints, ...dataPointsMerged, { y: used, label: "Số dư" }]);
+    }, [transactionsWallet])
     return (
         <Row>
             <Col span={12}>
                 <h1>All transactions by category from wallet {wallet.wallet_name}</h1>
-                <CanvasJSChart options={initOptions(dataPoints)} />
+                <CanvasJSChart options={setOptions(dataPoints)} />
             </Col>
             <Col span={12}>
                 <Statistic
--- frontend/src/utils/transaction.util.js
+++ frontend/src/utils/transaction.util.js
@@ -1,1 +1,21 @@
+import * as _ from 'lodash';
 export const formatNumberToMoney = value => value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+/**
+ * 
+ * @param {Array} transactions list transactions
+ * @param {Array} duplicated_lst contain transaction id which have multi transaction from one category
+ * @description If transaction is duplicated, it will be merged with total amount of all transactions
+ * @return {Object} new object 
+ * @example { label: 'category_name', y: 'amount_total' }
+ */
+export const mergeDupTransactionByCategory = (transactions, duplicated_lst) => {
+    return duplicated_lst.map(tran_cat => {
+        const transactions_by_cat = transactions.filter(tran => tran.category === tran_cat);
+        const amount_total =
+            transactions_by_cat
+                .map(tran => tran.action === "IN" ? parseFloat(tran.amount) : -parseFloat(tran.amount))
+                .reduce((prev, curr) => prev + curr, 0);
+        console.log("amount: " + amount_total);
+        return { label: transactions_by_cat[0].category_name, y: `${amount_total}` };
+    })
+}
