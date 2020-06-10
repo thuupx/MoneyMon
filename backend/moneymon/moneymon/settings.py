696aef2c1d547b9ade826bf5ff78cbad77cb5d09
vinhhung263 <hungpv263@gmail.com>
2020-06-10 15:01:26 +0700

    refactor(BE)

--- backend/moneymon/moneymon/settings.py
+++ backend/moneymon/moneymon/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'moneymon_db',
         'USER': 'postgres',
-        'PASSWORD': 'abc123',
+        'PASSWORD': 'vinhHung123',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
