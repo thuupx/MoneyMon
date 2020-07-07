a35bbd06aaa2a773719dd02845df41ba031a64f1
thupx <xuanthu404@gmail.com>
2020-07-07 08:31:29 +0700

    feat(FE): add pie chart

--- frontend/src/components/Statistic/Week.jsx
+++ frontend/src/components/Statistic/Week.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Col, DatePicker, Button, message } from 'antd';
-import { Bar } from 'react-chartjs-2';
+import { Bar, Pie } from 'react-chartjs-2';
 import moment from 'moment';
 import { getStatistics } from '../../api/statistic.api';
 import { IncomeDatasets, ExpenseDatasets } from './chart-option';
@@ -19,10 +19,31 @@ const _data = {
         }
     ]
 };
+const stylePie = {
+    backgroundColor: [
+        'rgba(75,192,192,0.8)',
+        'rgba(255,99,132,0.8)',
+    ],
+    hoverBackgroundColor: [
+        'rgba(75,192,192,1)',
+        'rgba(255,99,132,1)',
+    ]
+}
+const dataPie = {
+    labels: [
+        'Total Income',
+        'Total Expense',
+    ],
+    datasets: [{
+        data: [0, 0],
+        ...stylePie
+    }]
+};
 const dateFormat = 'YYYY-MM-DD';
 const currentDate = new Date().toJSON().slice(0, 10)//.replace(/-/g, '/');
 const WeekStatistic = props => {
     const [data, setData] = useState(_data);
+    const [statistic, setStatistic] = useState(dataPie);
     const fetchDatasets = (start_date, end_date) => {
         getStatistics({ start_date, end_date })
             .then(json => {
@@ -38,10 +59,15 @@ const WeekStatistic = props => {
                     console.log('dates', dates);
                     setData(prev => {
                         return {
-                            ...prev,
                             labels: dates,
                         }
-                    })
+                    });
+                    setStatistic(prev => ({
+                        datasets: [{
+                            data: [0, 0],
+                            ...stylePie
+                        }]
+                    }));
                     return;
                 }
                 const income = json.map(transaction => transaction.action === 'IN' && transaction.amount);
@@ -57,9 +83,22 @@ const WeekStatistic = props => {
                 }
                 setData(prev => {
                     return {
-                        ...prev,
                         labels: createdAt,
                         datasets: [dataIncome, dataExpense]
+                    }
+                });
+                const reducer = (acc, curr) => {
+                    if (!curr) curr = 0;
+                    return Number(acc) + Number(curr);
+                }
+                const totalIncome = income.reduce(reducer, 0);
+                const totalExpense = expense.reduce(reducer, 0);
+                setStatistic(prev => {
+                    return {
+                        datasets: [{
+                            data: [totalIncome, totalExpense],
+                            ...stylePie
+                        }]
                     }
                 })
             })
@@ -82,6 +121,7 @@ const WeekStatistic = props => {
             <Col xs={24} sm={24} md={8} lg={8}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate velit aliquid veritatis facere.
                 <RangePicker onChange={onChange} />
+                <Pie data={statistic} />
             </Col>
             <Col xs={24} sm={24} md={16} lg={16}>
                 <Bar data={data} />
